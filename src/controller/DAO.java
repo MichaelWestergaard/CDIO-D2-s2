8ca800ec9e3b6f1d66fb56d57ed7f942ca5bb25a
Alparslan B. Ari <burhanari@192.168.0.11>
2018-03-12 13:01:26 +0100

    InsertBatchnummer er færdig

--- src/controller/DAO.java
+++ src/controller/DAO.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import data.Batch;
 import data.User;
+import java.util.Scanner;
 
 
 public class DAO {
 
 	private List<User> users = new ArrayList<User>();	
 	private List<Batch> batches = new ArrayList<Batch>();
+	Scanner scan = new Scanner(System.in);
 	
 	public DAO () {		
 		users.add(new User(12, "Anders And"));
@@ -28,6 +30,42 @@ public class DAO {
 		return false;
 	}
 
-	
-	
+
+public boolean checkBatchId(int batchId) {
+	for(int i = 0; i < batches.size(); i++) {
+		if(batches.get(i).getBatchID() == batchId) {
+			return true;
+		}
+	}
+	return false;
 }
+
+
+public void insertBatchNo() {
+	int batchId = 0;
+
+	while(batchId == 0) {
+		System.out.println("Enter Batch Number (1000-9999): ");
+		int desiredID = scan.nextInt();
+
+		if(checkBatchId(desiredID) && desiredID >= 1000 && desiredID <= 9999) { //Skal have oprettet "checkUsername" i DAO. Skal returnere true, hvis brugernavnet er gyldigt og ledigt.
+			batchId = desiredID;
+		} else {
+			System.out.println("The Batch number, you entered, invalid.");
+		}
+	}
+
+}
+
+public boolean checkUnloaded() {
+	if(vægt==0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+}
+
+	
+	
+
